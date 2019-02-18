b36bb7c7a0059d33e846c360f95b32d1b4c208b9
omicronns <omicronns@gmail.com>
2019-02-18 19:22:44 +0100

    update test program

--- .test/main.cpp
+++ .test/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <vector>
 
 using namespace std::literals::chrono_literals;
 using std::this_thread::sleep_for;
 using std::cout;
 using std::endl;
+using std::vector;
 
 int main() {
 	auto val = 0;
+	auto vec = vector<int>{ 1, 2, 3 };
 	while (true) {
 		sleep_for(1s);
 		cout << "val: " << val++ << endl;
+		cout << "vec: " << vec[0]++ << endl;
 	}
 	return 0;
 }
